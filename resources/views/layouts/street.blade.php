24abbf8f2750710da05fbaefba2c4c1b11da53a5
muhamadijlal <haidarijlal027@gmail.com>
2022-08-25 20:41:46 +0700

    add some reference menu

--- resources/views/layouts/street.blade.php
+++ resources/views/layouts/street.blade.php
@@ -3,25 +3,25 @@
 @section('title','Street')
 
 @section('content')
-<section class="pt-16 dark:bg-dark">
-  <div class="pt-36 pb-32">
-    <div class="w-full relative lg:h-[500px] overflow-hidden">
+<section class="pt-0 dark:bg-dark">
+  <div class="pt-16 pb-12 lg:pb-32 ">
+    <div class="w-full relative h-96 lg:h-screen overflow-hidden bg-yellow-200">
       <div id="filterbackdropimg" class="w-full bg-dark h-full opacity-70 absolute z-1"></div>
-      <div class="w-full py-6 bottom-10 lg:top-[125px] absolute px-4">
-        <h1 class="text-white font-extralight text-5xl lg:text-8xl text-center">Street</h1> 
-        {{-- <p class="text-white font-semibold text-base lg:text-2xl text-center">Food & Baverages</p> --}}
+      <div class="w-full py-6 bottom-10 top-28 lg:top-48 absolute px-4">
+        <h1 class="text-white font-extralight text-2xl lg:text-8xl text-center">Street</h1> 
+        {{-- <p class="text-white font-semibold text-bse lg:text-2xl text-center">Food & Baverages</p> --}}
         <div class="flex items-center justify-center pt-5">
-          <input type="text" class="w-full lg:w-1/2 py-2 px-3 border border-primary focus:outline-none focus:ring-1 focus:ring-cyan-600 shadow-lg rounded-lg" placeholder="Cafe name, etc...">
-          <button type="submit" name="submit" class="bg-primary rounded-lg p-2 ml-2 hover:opacity-90 focus:ring-2 focus:ring-cyan-600">
+          <input type="text" class="w-full lg:w-1/2 py-1 px-3 lg:py-2 lg:px-3 border border-primary focus:outline-none focus:ring-1 focus:ring-cyan-600 shadow-lg rounded-lg" placeholder="Cafe name, etc...">
+          <button type="submit" name="submit" class="bg-primary rounded-lg p-2 ml-1 lg:ml-2 hover:opacity-90 focus:ring-2 focus:ring-cyan-600">
               <span class="text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="lg:h-6 lg:w-6 h-4 w-4 " fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
               </span>
             </button>
         </div>
       </div>
-      <img src="https://source.unsplash.com/1200x600?cafe" alt="heroSection" class="w-full">
+      <img src="https://source.unsplash.com/1200x600?cafe" alt="heroSection" class="w-full h-96 lg:h-screen">
     </div>
   </div>
 </section>
