f4bdd3aa7a6a1a6169d13c586de7493f80a5c119
muhamadijlal <haidarijlal027@gmail.com>
2022-09-19 06:18:24 +0700

    make other page

--- resources/views/layouts/other.blade.php
+++ resources/views/layouts/other.blade.php
@@ -2,9 +2,104 @@
 
 
 @section('content')
-    <section class="bg-dark pt-36 pb-32 px-20">
-      <div class="px-4 w-full">
-        <h1 class="text-4xl font-bold text-white">Others page</h1>
+{{-- Latest posts section start --}}
+<section id="fnb" class="pt-36 pb-32 dark:bg-dark">
+  <div class="container">
+    <div class="px-4 mb-20">
+      <h1 class="text-4xl text-dark dark:text-white font-bold">Other's Page</h1>
+    </div>
+    <div class="flex flex-wrap px-4">
+      <div class="w-full lg:w-1/3 lg:px-1 mb-2">
+        <div class="bg-white mb-10 overflow-hidden dark:bg-dark">
+          <img src="https://source.unsplash.com/300x300?programming" alt="#" class="w-full h-52">
+          <div class="py-6">
+            <div class="flex mb-5">
+              <img src="https://source.unsplash.com/300x300?person" alt="person" class="rounded-full h-7 w-7">
+              <h3 class="text-base font-semibold text-dark hover:text-primary truncate  ml-2 dark:text-white">Muhamad Haidar Ijlal</h3>
+            </div>
+            <h1 class="text-2xl font-bold text-dark mb-2 dark:text-white">Title</h1>
+            <p class="mb-10 h-24 text-secondary text-ellipsis overflow-hidden dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, quae quam officiis veniam blanditiis esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam eos dolore molestiae, dolorem libero, rerum odio aut sequi excepturi, cum omnis harum repellendus labore.</p>
+            <p class="text-xs text-secondary dark:text-white">2 March - Food  & Beverages</p>
+          </div>
+        </div>
       </div>
-    </section>
+      <div class="w-full lg:w-1/3 lg:px-1 mb-2">
+        <div class="bg-white mb-10 overflow-hidden dark:bg-dark">
+          <img src="https://source.unsplash.com/300x300?programming" alt="#" class="w-full h-52">
+          <div class="py-6">
+            <div class="flex mb-5">
+              <img src="https://source.unsplash.com/300x300?person" alt="person" class="rounded-full h-7 w-7">
+              <h3 class="text-base font-semibold text-dark hover:text-primary truncate  ml-2 dark:text-white">Muhamad Haidar Ijlal</h3>
+            </div>
+            <h1 class="text-2xl font-bold text-dark mb-2 dark:text-white">Title</h1>
+            <p class="mb-10 h-24 text-secondary text-ellipsis overflow-hidden dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, quae quam officiis veniam blanditiis esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam eos dolore molestiae, dolorem libero, rerum odio aut sequi excepturi, cum omnis harum repellendus labore.</p>
+            <p class="text-xs text-secondary dark:text-white">2 March - Food  & Beverages</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/3 lg:px-1 mb-2">
+        <div class="bg-white mb-10 overflow-hidden dark:bg-dark">
+          <img src="https://source.unsplash.com/300x300?programming" alt="#" class="w-full h-52">
+          <div class="py-6">
+            <div class="flex mb-5">
+              <img src="https://source.unsplash.com/300x300?person" alt="person" class="rounded-full h-7 w-7">
+              <h3 class="text-base font-semibold text-dark hover:text-primary truncate  ml-2 dark:text-white">Muhamad Haidar Ijlal</h3>
+            </div>
+            <h1 class="text-2xl font-bold text-dark mb-2 dark:text-white">Title</h1>
+            <p class="mb-10 h-24 text-secondary text-ellipsis overflow-hidden dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, quae quam officiis veniam blanditiis esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam eos dolore molestiae, dolorem libero, rerum odio aut sequi excepturi, cum omnis harum repellendus labore.</p>
+            <p class="text-xs text-secondary dark:text-white">2 March - Food  & Beverages</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/3 lg:px-1 mb-2">
+        <div class="bg-white mb-10 overflow-hidden dark:bg-dark">
+          <img src="https://source.unsplash.com/300x300?programming" alt="#" class="w-full h-52">
+          <div class="py-6">
+            <div class="flex mb-5">
+              <img src="https://source.unsplash.com/300x300?person" alt="person" class="rounded-full h-7 w-7">
+              <h3 class="text-base font-semibold text-dark hover:text-primary truncate  ml-2 dark:text-white">Muhamad Haidar Ijlal</h3>
+            </div>
+            <h1 class="text-2xl font-bold text-dark mb-2 dark:text-white">Title</h1>
+            <p class="mb-10 h-24 text-secondary text-ellipsis overflow-hidden dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, quae quam officiis veniam blanditiis esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam eos dolore molestiae, dolorem libero, rerum odio aut sequi excepturi, cum omnis harum repellendus labore.</p>
+            <p class="text-xs text-secondary dark:text-white">2 March - Food  & Beverages</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/3 lg:px-1 mb-2">
+        <div class="bg-white mb-10 overflow-hidden dark:bg-dark">
+          <img src="https://source.unsplash.com/300x300?programming" alt="#" class="w-full h-52">
+          <div class="py-6">
+            <div class="flex mb-5">
+              <img src="https://source.unsplash.com/300x300?person" alt="person" class="rounded-full h-7 w-7">
+              <h3 class="text-base font-semibold text-dark hover:text-primary truncate  ml-2 dark:text-white">Muhamad Haidar Ijlal</h3>
+            </div>
+            <h1 class="text-2xl font-bold text-dark mb-2 dark:text-white">Title</h1>
+            <p class="mb-10 h-24 text-secondary text-ellipsis overflow-hidden dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, quae quam officiis veniam blanditiis esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam eos dolore molestiae, dolorem libero, rerum odio aut sequi excepturi, cum omnis harum repellendus labore.</p>
+            <p class="text-xs text-secondary dark:text-white">2 March - Food  & Beverages</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/3 lg:px-1 mb-2">
+        <div class="bg-white mb-10 overflow-hidden dark:bg-dark">
+          <img src="https://source.unsplash.com/300x300?programming" alt="#" class="w-full h-52">
+          <div class="py-6">
+            <div class="flex mb-5">
+              <img src="https://source.unsplash.com/300x300?person" alt="person" class="rounded-full h-7 w-7">
+              <h3 class="text-base font-semibold text-dark hover:text-primary truncate  ml-2 dark:text-white">Muhamad Haidar Ijlal</h3>
+            </div>
+            <h1 class="text-2xl font-bold text-dark mb-2 dark:text-white">Title</h1>
+            <p class="mb-10 h-24 text-secondary text-ellipsis overflow-hidden dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, quae quam officiis veniam blanditiis esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam eos dolore molestiae, dolorem libero, rerum odio aut sequi excepturi, cum omnis harum repellendus labore.</p>
+            <p class="text-xs text-secondary dark:text-white">2 March - Food  & Beverages</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-full flex justify-end">
+      <p class="text-base block font-semibold text-primary hover:text-dark dark:text-white dark:hover:text-primary cursor-pointer">
+        <a href="#">Show more ></a>
+      </p>
+    </div>
+  </div>
+</section>
+{{-- Latest posts section end --}}
 @endsection
